109fc88ab54dfe82cc29285c2e692adca6d62759
zinebnadak <zinebnadak04@outlook.com>
2026-02-22 14:26:27 +0200

    22.2

---
+++ JavaScript/JavaScript (theory)/JavaScript_Intro.js
@@ -0,0 +1,27 @@
+// From website to webbapplication (only the frontend part)
+
+// What will we built with JavaScript?
+
+/*
+SYSTEM FUNCTIONALITY OVERVIEW
+(only the frontend part, no backend or database connection in this course))
+
+1. Authentication System
+   - Secure login required to access internal features.
+
+2. Personnel Register
+   - Add new staff members
+   - Update existing employee information
+   - Delete employee records
+
+3. Integrated Calculator
+   - Built-in calculation tool for internal use
+
+4. Headquarters Virus Database
+   - Add new research data
+   - Update existing research entries
+   - Delete outdated or incorrect data
+
+5. Internal Team Chat
+   - Communication platform for employees
+*/
---
+++ JavaScript/JavaScript (theory)/JavaScript_basics.js
@@ -0,0 +1,29 @@
+
+/* About JavaScript
+
+JavaScript is a programming language that allows you to create dynamic and interactive web pages.
+It is one of the core technologies of the web, alongside HTML and CSS.
+
+Just-in-time-compilation JIT = JavaScript runs directly in the browser at runtime (line by line) instead of being precompiled before execution. -->
+compare to AOT (ahead-of-time compilation) = JavaScript code is compiled into machine code before execution, which can improve performance. Languages like C++ or Rust are compiled before the program runs -->
+
+Call Stack = Checklist / to-do list of tasks to be completed, including the execution order of functions and their parameters.
+Stack = Last In, First Out (LIFO) data structure used to manage function calls and their execution context. When a function is called, it is added to the top of the stack, and when it finishes executing, it is removed from the stack. This allows JavaScript to keep track of which function is currently executing and what functions are waiting to be executed next.
+
+JavaScript achieves asynchronous behavior through the combination of Web APIs, the Callback (Task) Queue, and the Event Loop coordinating execution with the Call Stack. -->
+
+
+/*
+DOM (Document Object Model)
+
+The DOM is the browser’s internal representation of an HTML page.
+It converts the HTML structure into a tree of objects (nodes)
+that JavaScript can read, modify, add to, or delete.
+
+In short: The DOM is what allows JavaScript to interact with the page.
+
+
+
+
+
+
